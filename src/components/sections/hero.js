ce96545c133e7adf14bbeecadd05ea826a6237a4
suman shah <sumanshahofficial@gmail.com>
2024-10-05 11:05:59 +1000

    Initial Commit

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -76,10 +76,10 @@ const Hero = () => {
   const five = (
     <a
       className="email-link"
-      href="https://www.newline.co/courses/build-a-spotify-connected-app"
+      href="{`mailto:${email}`}"
       target="_blank"
       rel="noreferrer">
-      Check out my course!
+      Contact me
     </a>
   );
 
